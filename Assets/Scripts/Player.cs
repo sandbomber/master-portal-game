095690819313c756607c4c2c39f8c4f632753f97
sandbomber <60323670+sandbomber@users.noreply.github.com>
2020-01-26 10:01:33 -0800

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject orangePortalPrefab;
+    public GameObject bluePortalPrefab;
+
+    public GameObject orangePortal;
+    public GameObject bluePortal;
+
+    public float speed = 5f;
+
+    float lookHorizontal;
+    float lookVertical;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.Impulse);
+        }
+
+        lookHorizontal += Input.GetAxis("Mouse X");
+        lookVertical += Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(-lookVertical, lookHorizontal, 0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray();
+            ray.origin = Camera.main.transform.position;
+            ray.direction = Camera.main.transform.forward;
+            Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 3f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                Quaternion rotation = Quaternion.LookRotation(hit.normal, Vector3.up);
+
+                if (orangePortal != null)
+                {
+                    Destroy(orangePortal);
+                }
+                orangePortal = (GameObject)Instantiate(orangePortalPrefab, hit.point, rotation);
+
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = new Ray();
+            ray.origin = Camera.main.transform.position;
+            ray.direction = Camera.main.transform.forward;
+            Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 3f);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                Quaternion rotation = Quaternion.LookRotation(hit.normal, Vector3.up);
+
+                if (bluePortal != null)
+                {
+                    Destroy(bluePortal);
+                }
+                bluePortal = (GameObject)Instantiate(bluePortalPrefab, hit.point, rotation);
+
+            }
+        }
+	}
+}
