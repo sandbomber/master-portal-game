e48f90527e7ea95948f0430b1cbbb075f911c90f
sandbomber <60323670+sandbomber@users.noreply.github.com>
2020-01-26 10:07:31 -0800

    Add files via upload

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 3 * Time.deltaTime);
+	}
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            coll.gameObject.GetComponent<Rigidbody>().AddForce(
+                Vector3.forward * 50,
+                ForceMode.Impulse);
+        }
+    }
+}
---
+++ Scripts/Portal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    public Transform exit;
+
+    public string otherPortalTag;
+    public GameObject otherPortal;
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            otherPortal = GameObject.FindGameObjectWithTag(otherPortalTag);
+            if (otherPortal != null)
+            {
+                coll.transform.position = otherPortal.GetComponent<Portal>().exit.position;
+            }
+        }
+    }
+}
